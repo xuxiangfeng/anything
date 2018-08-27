5c41825d02451babee3e1905e0116bc5d6886451
xxf <whutxxf@163.com>
2018-08-27 18:35:44 +0800

    初始化项目结构
    初始化项目结构

---
+++ anything-entity/src/main/java/org/whut/xxf/anything/entity/AnythingEntityApplication.java
@@ -0,0 +1,12 @@
+package org.whut.xxf.anything.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnythingEntityApplication.class, args);
+	}
+}
---
+++ src/main/java/org/whut/xxf/anything/AnythingApplication.java
@@ -0,0 +1,12 @@
+package org.whut.xxf.anything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnythingApplication.class, args);
+	}
+}
